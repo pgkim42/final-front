2fd4da7bb8f4795b09937cb0014bb30990e02607
PG Kim <vakon1@daum.net>
2024-12-19 00:19:03 +0900

    ...

--- src/features/user/apis/index.ts
+++ src/features/user/apis/index.ts
@@ -15,8 +15,8 @@ const errorHandler = (error:any) => {
   return responseBody;
 }
 
-const DOMAIN = 'http://localhost:8080';
-const DOMAIN_AWS = 'http://13.125.68.182:8080'; // your aws address
+// const DOMAIN = 'http://localhost:8080';
+const DOMAIN = 'http://13.125.68.182:8080'; // your aws address
 const API_DOMAIN = `${DOMAIN}/api/v1`;
 
 export const SNS_SIGN_IN_URL = (type: 'kakao' | 'naver') => `${API_DOMAIN}/auth/oauth2/${type}`;
